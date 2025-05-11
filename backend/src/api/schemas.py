adb401710428c8e9b7b4df14dd9cc2e1ec82b287
TomiGarbe <2105905ucc.edu.ar>
2025-05-10 22:25:25 -0300

    feat(usuarios): Configuracion de autenticacion en frontend - Agregamos variable de entorno para firebase - Agregamos codigo de creacion de usuario admin - Actualizamos modelos de usuario y cuadrilla para uso de id en firebase - Agregamos codigo de configuracion de firebase y auth - Actualizamos rutas, esquemas, controladores y servicios de sucursales y zonas - Creamos controladores y servicios para usuarios y cuadrillas
    Refs: #4

--- backend/src/api/schemas.py
+++ backend/src/api/schemas.py
@@ -1,5 +1,36 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
+from enum import Enum
+from datetime import date, datetime
 from typing import Optional
+
+# Enum para los roles
+class Role(str, Enum):
+    ADMIN = "Administrador"
+    ENCARGADO = "Encargado de Mantenimiento"
+
+# Esquemas para Usuario
+class UserCreate(BaseModel):
+    nombre: str
+    email: EmailStr
+    contrasena: Optional[str] = None
+    rol: Role
+
+class UserUpdate(BaseModel):
+    nombre: Optional[str] = None
+    email: Optional[EmailStr] = None
+    rol: Optional[Role] = None
+
+# Esquemas para Cuadrilla
+class CuadrillaCreate(BaseModel):
+    nombre: str
+    zona: str
+    email: EmailStr
+    contrasena: Optional[str] = None
+
+class CuadrillaUpdate(BaseModel):
+    nombre: Optional[str] = None
+    zona: Optional[str] = None
+    email: Optional[EmailStr] = None
 
 # Esquemas para Zona
 class Zona(BaseModel):
