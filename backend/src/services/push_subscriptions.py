d35b9324342fbec1283046a47e7e56485e0adfc5
TomiGarbe <2105905@ucc.edu.ar>
2025-08-01 23:21:55 -0300

    fix(chats y notificaciones): Corregimos error al eliminar suscripcion de notificaciones push y corregimos error de carga de mensajes.

--- backend/src/services/push_subscriptions.py
+++ backend/src/services/push_subscriptions.py
@@ -1,8 +1,7 @@
 from fastapi import HTTPException
 from sqlalchemy.orm import Session
 from api.models import PushSubscription
-from api.schemas import PushSubscriptionCreate, PushSubscriptionDelete
-
+from api.schemas import PushSubscriptionCreate
 
 def save_subscription(db_session: Session, current_entity: dict, sub: PushSubscriptionCreate):
     if not current_entity:
@@ -28,11 +27,14 @@ def get_subscriptions(db_session: Session, firebase_uid: str):
     return db_session.query(PushSubscription).filter(PushSubscription.firebase_uid == firebase_uid).all()
 
 
-def delete_subscription(db_session: Session, sub: PushSubscriptionDelete):
-    subs = db_session.query(PushSubscription).filter(PushSubscription.firebase_uid == sub.firebase_uid and PushSubscription.device_info == sub.device_info).all()
-    if not subs:
-        raise HTTPException(status_code=404, detail="Subscriptions not found")
-    for sub in subs:
-        db_session.delete(sub)
-    db_session.commit()
-    return {"message": "Subscription deleted"}
+def delete_subscription(db_session: Session, firebase_uid: str, device_info: str):
+    try:
+        subs = db_session.query(PushSubscription).filter(PushSubscription.firebase_uid == firebase_uid, PushSubscription.device_info == device_info,).all()
+        if not subs:
+            raise HTTPException(status_code=404, detail="Subscriptions not found")
+        for sub in subs:
+            db_session.delete(sub)
+        db_session.commit()
+        return {"message": "Subscription deleted"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error interno: {str(e)}")
