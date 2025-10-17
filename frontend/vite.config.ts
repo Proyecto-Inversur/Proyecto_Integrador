ea4d05c6349cbf5bc0fed8feaa3f01468bdf44db
fgazzera <2113461@ucc.edu.ar>
2025-10-17 11:26:04 -0300

    feat(tests): agregar pruebas unitarias para hooks de lógica de negocio
    Hooks Autenticación (`useAuth`, `useAuthRoles`):
    - Implementa tests para useAuth, validando los estados de carga, verificación, cierre de sesión y manejo de errores de autenticación.
    - Añade tests para useAuthRoles, verificando los roles de usuario, cuadrilla y administrador.
    
    Hooks Formularios (`useCuadrillas`, `useSucursales`, `useUsers`): Se añaden pruebas para los hooks de gestión de entidades, asegurando la carga, edición, eliminación de datos y el manejo del estado del formulario.
    
    Hooks Mantenimientos (`useCorrectivo`, `usePreventivo`, `useMantenimientos`): Se crean tests para validar la lógica de finalización, la gestión de la ruta del mapa y el manejo del modal de imágenes.
    
    Hooks Mapas (`useMapsData`, `usePopups`, `useRutaData`): Se prueban los hooks de datos de mapas para verificar la obtención, procesamiento y filtrado de datos geográficos.
    
    Hook Chat (`useChat`*: Se valida la suscripción y limpieza de la conexión del WebSocket.
    
    - Configuración `vite.config.ts`: Se actualiza la configuración de Vitest para mejorar la generación de reportes de cobertura, excluyendo archivos y carpetas no relevantes para las pruebas unitarias.
    
    refs: #10

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -15,23 +15,33 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: ['./tests/setup.ts'],     
     include: ['tests/**/*.{test,spec}.{js,ts,jsx,tsx}'],
-    coverage: {
+        coverage: {
       provider: 'v8',
-      reporter: ['text', 'json', 'html'],
+      reporter: ['text', 'html'],
       reportsDirectory: './tests/coverage',
-      
       include: ['src/**/*.{js,jsx,ts,tsx}'],
-
+      
+      // Aca agregamos los archivos y carpetas a excluir.
       exclude: [
         'src/main.tsx',
         'src/main.jsx',
         'src/config.js',
-        'src/vite-env.d.ts',
-        'src/context/', // Los contextos suelen ser solo "boilerplate"
-        'src/routes.jsx', // Es un archivo de configuración
+        
+        // Excluir toda la carpeta de servicios
+        'src/services/',
+
+        // Excluir hooks específicos del mapa
+        'src/hooks/maps/useLeafletMap.js',
+        'src/hooks/maps/useMapa.jsx',
+        'src/hooks/maps/useMapRoutes.js',
+        'src/hooks/maps/useRutaNavegacion.js',
+
+        'src/utils/',
+
+
+        'src/context/',
+        'src/routes.jsx',
         'src/styles/',
-        'src/assets/',
-        '**/__mocks__/**',
         '**/node_modules/**',
         '**/tests/**',
       ],
